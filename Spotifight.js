12ddfe4bfecb1290b85f26464f001292f631ad47
majstroll <nithander@gmail.com>
2014-08-05 16:11:14 +0200

    Gött med kul

--- Spotifight.js
+++ Spotifight.js
@@ -1,9 +1,20 @@
 Cards = new Meteor.Collection('cards');
 
+var clientID = "6da341adbed94c3ea669dfa249804410";
+var clientSecret = "449b5590a223410dbf402fe7e174199b";
+var redirectURI = 'http://localhost:3000'; // Your redirect uri
+
 if (Meteor.isClient) {
   Template.cards.helpers({
     cards: function() {
       return Cards.find();
+    }
+  });
+
+  Template.generation.events({
+    'click #initButton': function(event) {
+      console.log("ttjo");
+      initiate();
     }
   });
 }
@@ -16,4 +27,56 @@ if (Meteor.isServer) {
     Cards.insert( {cardname: "Mick Jagger", attack: 5, defense: 5, url: "http://4.bp.blogspot.com/-dtv-4JoDabU/T-_onzjcwHI/AAAAAAAAAbU/TuG902lIGME/s320/mick-jagger-old_pic4_us1.jpg" } );
     Cards.insert( {cardname: "Def Leppard Drummer", attack: 14, defense: 1, url: "http://s3.amazonaws.com/rapgenius/public-transport-guide.jpg" } );    
   });
+}
+
+function initiate() {
+  console.log("tjenis");
+ var user_id = $('#userID').val();
+ var playlist_id = $('#playlistID').val(); 
+ console.log("din mamma");
+ 
+ $.ajax({
+  url: 'https://accounts.spotify.com/authorize',
+  headers : {'Access-Control-Allow-Origin': redirectURI},
+  data: {
+    client_id : clientID,
+    response_type : 'code',
+    redirect_uri: redirectURI
+  },
+  success: function(response){ 
+    $.ajax({
+      url: 'https://accounts.spotify.com/api/token',
+      crossDomain: true,
+      data: {
+        grant_type : 'authorization_code',
+        code : response.code,
+        redirect_uri : redirectURI,
+        client_id : clientID,
+        client_secret : clientSecret
+      },
+      success: function(response) {
+        console.log(response)
+        var accessToken = response.access_token;
+        generateCards(user_id,playlist_id,accessToken);
+      }
+    })
+  }
+ })
+}
+
+function generateCards(user_id,playlist_id,accessToken) {
+  console.log("nein");
+  $.ajax({
+                url: 'https://api.spotify.com/v1/users/' + user_id + '/playlist/' + playlist_id + '/tracks',
+                headers: {
+                    'Authorization': 'Bearer ' + accessToken
+                },
+                success: function(response) {
+                    console.log(response);
+                    tracks = response.items
+                    for (var track in tracks) {
+                      Cards.insert ({cardname: track.name, attack: track.popularity, defense: (100 - track.popularity), url: "lulz.com"})
+                    }
+                }
+            });
 }
